5c900498cafc18c12ba6dd85c0f4d62617cac499
DYung26 <theadstore1407@gmail.com>
2024-10-28 21:04:22 -0700

    Fix `get_hyper` method to accurately reflect page size based on `data` length instead of returning `0` for empty data

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -71,7 +71,7 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, int]:
         data = self.get_page(page, page_size)
         return {
-            'page_size': page_size if data else 0, 'page': page,
+            'page_size': len(data), 'page': page,
             'data': data,
             'next_page': page+1 if self.get_page(page+1, page_size) else None,
             'prev_page': page-1 if page-1 != 0 else None,
