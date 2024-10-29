8b54201e2ab7dbbe126172edbaa01610fe7115ef
DYung26 <theadstore1407@gmail.com>
2024-10-28 21:01:34 -0700

    Refactor `get_hyper method` to use retrieved data for pagination calculations, improving handling of empty responses.

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -69,9 +69,10 @@ class Server:
         return dataset[start_idx:end_idx] if start_idx < len(dataset) else []
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, int]:
+        data = self.get_page(page, page_size)
         return {
-            'page_size': page_size, 'page': page,
-            'data': self.get_page(page, page_size),
+            'page_size': page_size if data else 0, 'page': page,
+            'data': data,
             'next_page': page+1 if self.get_page(page+1, page_size) else None,
             'prev_page': page-1 if page-1 != 0 else None,
             'total_pages': math.ceil(len(self.dataset())/page_size)
